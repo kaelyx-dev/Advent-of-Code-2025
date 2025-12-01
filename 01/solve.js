ab246ad8e6d07cc8d0bf7c8d5730d70be77cad9c
Kaelyx <43659841+kaelyx-dev@users.noreply.github.com>
2025-12-01 14:57:31 +0000

    Day 1 - Part A + Part B in a single line of javascript*

---
+++ 01/solve.js
@@ -0,0 +1,7 @@
+import fs from 'fs'
+
+const solve = (fileName) => fs.readFileSync(fileName, "utf8").trim().split("\n").map(line => ({d: line[0], c: parseInt(line.slice(1).trim(), 10)})).reduce(({_d, a, b}, {d, c}) => { d === "L" && (c *= -1, b += (((_d + c) % 100 === 0 ? 1 : 0) - (_d % 100 === 0 ? 1 : 0))); b += Math.abs(Math.floor(_d / 100) - Math.floor((_d += c) / 100)) + ((_d % 100 === 0) ? (a += 1, 0) : 0); return {_d, a, b};}, {_d: 50, a: 0, b: 0});
+
+const result = solve("input.txt");
+console.log("Part A:", result.a);
+console.log("Part B:", result.b);

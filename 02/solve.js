6f1616b580c8fedf7fbb19eec707fd0b18b5be4a
Kaelyx <scott-donaldson@outlook.com>
2025-12-02 15:18:40 +0000

    Quicker... shorter... stronger... faster

--- 02/solve.js
+++ 02/solve.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 // const solve = f => fs.readFileSync(f, "utf8").split(",").map(e => e.split("-").map(Number)).map(([s, e]) => Array.from({ length: e - s + 1 }, (_, i) => i + s)).map(arr => ({a: arr.filter(n => /^(\d+)\1$/.test(n+"")), b: arr.filter(n => /^(\d+)\1+$/.test(n+""))})).reduce((r, o) => ({a: r.a + o.a.reduce((x, y) => x + y, 0),b: r.b + o.b.reduce((x, y) => x + y, 0)}),{ a: 0, b: 0 });
 
-const solve = f => fs.readFileSync(f,"utf8").split(",").map(e=>e.split("-").map(Number)).reduce((r,[s,e])=>{for(let n=s,t;n<=e;n++)t=n+"",/^(\d+)\1$/.test(t)&&(r.a+=n),/^(\d+)\1+$/.test(t)&&(r.b+=n);return r},{a:0,b:0});
+const solve = f => fs.readFileSync(f,"utf8").split(",").map(e=>e.split("-").map(Number)).reduce(({a,b},[s,e])=>{for(let n=s,t;n<=e;n++)t=n+"",/^(\d+)\1$/.test(t)&&(a+=n),/^(\d+)\1+$/.test(t)&&(b+=n);return {a,b}},{a:0,b:0});
 
 let start = performance.now();
 const result = solve("input.txt");

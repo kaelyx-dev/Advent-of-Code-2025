e776f46ef369af5d7abc976926add5f40b1896da
Kaelyx <scott-donaldson@outlook.com>
2025-12-02 11:45:09 +0000

    Changed output to be "nicer"

--- 01/solve.js
+++ 01/solve.js
@@ -1,5 +1,4 @@
-import fs from 'fs'
-
+const fs = require("fs");
 const solve = f => fs.readFileSync(f, "utf8").split("\n").map(l => ({d: l[0], c: +l.slice(1)})).reduce(({_d, a, b}, {d, c}) => { d == "L" && (c *= -1, b += (((_d + c) % 100 == 0 ? 1 : 0) - (_d % 100 == 0 ? 1 : 0))); b += Math.abs(Math.floor(_d / 100) - Math.floor((_d += c) / 100)) + ((_d % 100 == 0) ? (a++, 0) : 0); return {_d, a, b};}, {_d: 50, a: 0, b: 0});
 
 const result = solve("input.txt");
--- 02/solve.js
+++ 02/solve.js
@@ -2,4 +2,7 @@ const fs = require('node:fs');
 
 const solve = f => fs.readFileSync(f, "utf8").split(",").map(e => e.split("-").map(e => +e)).map(([s, e]) => Array.from({ length: e - s + 1 }, (_, i) => i + s)).map(arr => ({a: arr.filter(n => /^(\d+)\1$/.test(n+"")), b: arr.filter(n => /^(\d+)\1+$/.test(n+""))})).reduce((r, o) => ({a: r.a + o.a.reduce((x, y) => x + y, 0),b: r.b + o.b.reduce((x, y) => x + y, 0)}),{ a: 0, b: 0 });
 
-console.log(solve("input.txt"));
+const result = solve("input.txt");
+console.log("Part A:", result.a);
+console.log("Part B:", result.b);
+

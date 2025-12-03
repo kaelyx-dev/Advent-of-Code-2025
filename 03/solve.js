f86f07b48f206de4fd16b4dad0b6867b5f2d0d9b
Kaelyx <scott-donaldson@outlook.com>
2025-12-03 10:23:57 +0000

    Updated day 3 to show nicer output and execution time

--- 03/solve.js
+++ 03/solve.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 
 const solve = f => fs.readFileSync(f,"utf8").split("\r\n").map(s => ((z=(s,n)=>+[...Array(n)].reduce((o,_,k)=>((w=s.slice(o.i,s.length-(n-k)+1)),(x=Math.max(...w)),{r:o.r+x, i:o.i+w.indexOf(String(x))+1}),{r:"",i:0}).r),{a:z(s,2) ,b: z(s,12)})).reduce((r, o)=>({ a: r.a + o.a, b: r.b + (o.b || 0)}),{a:0,b:0});
+const start = performance.now();
+const result = solve("input.txt")
+const end = performance.now();
 
-console.log(solve("input.txt"))
+console.log(`Execution time: ${end - start} ms`);
+console.log("Part A:", result.a);
+console.log("Part B:", result.b);

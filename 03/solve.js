c3ce773dc4239122207835fb049dc5a94f554529
Kaelyx <scott-donaldson@outlook.com>
2025-12-03 10:20:33 +0000

    Day 3

---
+++ 03/solve.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+const solve = f => fs.readFileSync(f,"utf8").split("\r\n").map(s => ((z=(s,n)=>+[...Array(n)].reduce((o,_,k)=>((w=s.slice(o.i,s.length-(n-k)+1)),(x=Math.max(...w)),{r:o.r+x, i:o.i+w.indexOf(String(x))+1}),{r:"",i:0}).r),{a:z(s,2) ,b: z(s,12)})).reduce((r, o)=>({ a: r.a + o.a, b: r.b + (o.b || 0)}),{a:0,b:0});
+
+console.log(solve("input.txt"))

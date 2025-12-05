f1bb27de22d10be954c440d38b6b46ca5f7bf279
Kaelyx <scott-donaldson@outlook.com>
2025-12-05 14:11:11 +0000

    Its a little faster

--- 05/solve.js
+++ 05/solve.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const solve = f => (o={a:0,b:0},_s="\r\n",_j="-",[r,i]=fs.readFileSync(f, "utf-8").split(_s+_s).map(e=>e.split(_s)),r=(m=[],r.map(e=>e.split(_j).map(Number)).sort((a,b)=>a[0]-b[0]).forEach(([s,e])=>(!m.length||s>m[m.length-1][1])?m.push([s,e]):m[m.length-1][1]=Math.max(m[m.length-1][1],e)), m),o.b=r.reduce((a,[x,y])=>a+y-x+1,0),o.a=i.map(e=>((v,r,f=(l=0,h=r.length-1)=>l>h?0:((m=l+h>>1),([s,e]=r[m]),v<s?f(l,m-1):v>e?f(m+1,h):1))=>f())(e,r)).reduce((a,b)=>a+b,0),o)
+const solve=f=>(o={a:0,b:0},_s="\r\n",_j="-",[r,i]=fs.readFileSync(f, "utf-8").split(_s+_s).map(e=>e.split(_s)),r=(m=[],r.map(e=>e.split(_j).map(Number)).sort((a,b)=>a[0]-b[0]).forEach(([s,e])=>(!m.length||s>m[m.length-1][1])?m.push([s,e]):m[m.length-1][1]=Math.max(m[m.length-1][1],e)), m),o.b=r.reduce((a,[x,y])=>a+y-x+1,0),i.map(e=>((v,r,f=(l=0,h=r.length-1)=>l>h?0:((m=l+h>>1),([s,e]=r[m]),v<s?f(l,m-1):v>e?f(m+1,h):o.a++))=>f())(e,r)),o)
 
 const start = performance.now();
 const result = solve("input.txt")
